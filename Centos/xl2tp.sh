55368db3671fc2c0b21a8d85ef219c0289825714
Valley <36614122+aryayk@users.noreply.github.com>
2020-11-05 14:19:46 +0800

    Update xl2tp.sh

--- Centos/xl2tp.sh
+++ Centos/xl2tp.sh
@@ -1,1 +1,9 @@
+#### Prepare tools ####
+yum -y install vim lrzsz bash-completion net-tools wget gcc
+sed -i '7s/enforcing/disabled/' /etc/sysconfig/selinux
+setenforce 0
 
+#### Synchronization time ####
+sed -i '2s/pool/#pool/' /etc/chrony.conf
+sed -i '3s/server ntp.aliyun.com iburst'
+systemctl restart chronyd
